fcce7c022f5d214df6f60505722d0b4083ec16da
Erlan Marinho <erlan.marinho1125@gmail.com>
2025-09-06 20:58:49 -0300

    add first component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { NgOptimizedImage } from "@angular/common";
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, NgOptimizedImage],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
