aad2837536520e6f42c965346530519f833f61a7
Erlan Marinho <erlan.marinho1125@gmail.com>
2025-09-07 21:20:54 -0300

    we added the component to home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { NgOptimizedImage } from "@angular/common";
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { NbtnPrimaryComponent } from "../nbtn-primary/nbtn-primary.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage, NbtnPrimaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
